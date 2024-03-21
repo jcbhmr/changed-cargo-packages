7bd9a559ad376649257ea8b38c6c738f97c7851e
null <null>
2024-03-21 11:33:03 -0500

    save

--- jest/setupEnv.cjs
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-process.env.TESTING = "1"
-process.env.GITHUB_WORKSPACE = path.join(
-  path.resolve(__dirname, '..'), '.'
-)
-process.env.GITHUB_ACTION_PATH = path.join(
-  path.resolve(__dirname, '..'), '.'
-)
--- src/main.ts
+++ src/main.ts
@@ -1,290 +1,44 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import path from 'path'
-import {
-  processChangedFiles,
-  ChangeTypeEnum,
-  getAllDiffFiles,
-  getChangedFilesFromGithubAPI,
-  getRenamedFiles
-} from './changedFiles'
-import {
-  DiffResult,
-  getSHAForNonPullRequestEvent,
-  getSHAForPullRequestEvent
-} from './commitSha'
-import {Env, getEnv} from './env'
-import {getInputs, Inputs} from './inputs'
-import {
-  getFilePatterns,
-  getRecoverFilePatterns,
-  getSubmodulePath,
-  getYamlFilePatterns,
-  hasLocalGitDirectory,
-  isRepoShallow,
-  recoverDeletedFiles,
-  setOutput,
-  submoduleExists,
-  updateGitGlobalConfig,
-  verifyMinimumGitVersion,
-  warnUnsupportedRESTAPIInputs
-} from './utils'
+import { sep } from "node:path"
+import * as core from "@actions/core"
+import * as github from "@actions/github"
+import { $ } from "execa"
+import {PushEvent} from '@octokit/webhooks-types/schema'
 
-const getChangedFilesFromLocalGitHistory = async ({
-  inputs,
-  env,
-  workingDirectory,
-  filePatterns,
-  yamlFilePatterns
-}: {
-  inputs: Inputs
-  env: Env
-  workingDirectory: string
-  filePatterns: string[]
-  yamlFilePatterns: Record<string, string[]>
-}): Promise<void> => {
-  await verifyMinimumGitVersion()
+const token = core.getInput("token")
+const octokit = github.getOctokit(token)
 
-  let quotepathValue = 'on'
-
-  if (!inputs.quotepath) {
-    quotepathValue = 'off'
-  }
-
-  await updateGitGlobalConfig({
-    name: 'core.quotepath',
-    value: quotepathValue
-  })
-
-  if (inputs.diffRelative) {
-    await updateGitGlobalConfig({
-      name: 'diff.relative',
-      value: 'true'
-    })
-  }
-
-  const isShallow = await isRepoShallow({cwd: workingDirectory})
-  const hasSubmodule = await submoduleExists({cwd: workingDirectory})
-  let gitFetchExtraArgs = ['--no-tags', '--prune', '--recurse-submodules']
-  const isTag = env.GITHUB_REF?.startsWith('refs/tags/')
-  const outputRenamedFilesAsDeletedAndAdded =
-    inputs.outputRenamedFilesAsDeletedAndAdded
-  let submodulePaths: string[] = []
-
-  if (hasSubmodule) {
-    submodulePaths = await getSubmodulePath({cwd: workingDirectory})
-  }
-
-  if (isTag) {
-    gitFetchExtraArgs = ['--prune', '--no-recurse-submodules']
-  }
-
-  let diffResult: DiffResult
-
-  if (!github.context.payload.pull_request?.base?.ref) {
-    core.info(`Running on a ${github.context.eventName || 'push'} event...`)
-    diffResult = await getSHAForNonPullRequestEvent(
-      inputs,
-      env,
-      workingDirectory,
-      isShallow,
-      hasSubmodule,
-      gitFetchExtraArgs,
-      isTag
-    )
-  } else {
-    core.info(
-      `Running on a ${github.context.eventName || 'pull_request'} (${
-        github.context.payload.action
-      }) event...`
-    )
-    diffResult = await getSHAForPullRequestEvent(
-      inputs,
-      env,
-      workingDirectory,
-      isShallow,
-      hasSubmodule,
-      gitFetchExtraArgs
-    )
-  }
-
-  if (diffResult.initialCommit) {
-    core.info('This is the first commit for this repository; exiting...')
-    core.endGroup()
-    return
-  }
-
-  core.info(
-    `Retrieving changes between ${diffResult.previousSha} (${diffResult.targetBranch}) → ${diffResult.currentSha} (${diffResult.currentBranch})`
-  )
-
-  const allDiffFiles = await getAllDiffFiles({
-    workingDirectory,
-    hasSubmodule,
-    diffResult,
-    submodulePaths,
-    outputRenamedFilesAsDeletedAndAdded,
-    fetchAdditionalSubmoduleHistory: inputs.fetchAdditionalSubmoduleHistory,
-    failOnInitialDiffError: inputs.failOnInitialDiffError,
-    failOnSubmoduleDiffError: inputs.failOnSubmoduleDiffError
-  })
-  core.debug(`All diff files: ${JSON.stringify(allDiffFiles)}`)
-  core.info('All Done!')
-  core.endGroup()
-
-  if (inputs.recoverDeletedFiles) {
-    let recoverPatterns = getRecoverFilePatterns({inputs})
-
-    if (recoverPatterns.length > 0 && filePatterns.length > 0) {
-      core.info('No recover patterns found; defaulting to file patterns')
-      recoverPatterns = filePatterns
+let changedFiles: string[]
+if (await $`git status`.then(() => true, () => false)) {
+    const { stdout } = await $`git diff --name-only HEAD^`
+    changedFiles = stdout.split(/\r?\n/g).filter(x => x)
+} else {
+    if (github.context.eventName === "push") {
+        const payload = github.context.payload as PushEvent
+        changedFiles = payload.commits.reduce<string[]>((a, x) => a.concat(x.added, x.modified, x.removed), [])
+    } else if (github.context.eventName === "pull_request") {
+        const { data } = await octokit.rest.pulls.listFiles({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: github.context.payload.pull_request!.number
+        })
+        changedFiles = data.flatMap(x => x.previous_filename ? [x.previous_filename, x.filename] : x.filename)
+    } else {
+        throw new DOMException(`cannot handle ${github.context.eventName}`)
     }
-
-    await recoverDeletedFiles({
-      inputs,
-      workingDirectory,
-      deletedFiles: allDiffFiles[ChangeTypeEnum.Deleted],
-      recoverPatterns,
-      diffResult,
-      hasSubmodule,
-      submodulePaths
-    })
-  }
-
-  await processChangedFiles({
-    filePatterns,
-    allDiffFiles,
-    inputs,
-    yamlFilePatterns,
-    workingDirectory
-  })
-
-  if (inputs.includeAllOldNewRenamedFiles) {
-    core.startGroup('changed-files-all-old-new-renamed-files')
-    const allOldNewRenamedFiles = await getRenamedFiles({
-      inputs,
-      workingDirectory,
-      hasSubmodule,
-      diffResult,
-      submodulePaths
-    })
-    core.debug(`All old new renamed files: ${allOldNewRenamedFiles}`)
-    await setOutput({
-      key: 'all_old_new_renamed_files',
-      value: allOldNewRenamedFiles.paths,
-      writeOutputFiles: inputs.writeOutputFiles,
-      outputDir: inputs.outputDir,
-      json: inputs.json,
-      safeOutput: inputs.safeOutput
-    })
-    await setOutput({
-      key: 'all_old_new_renamed_files_count',
-      value: allOldNewRenamedFiles.count,
-      writeOutputFiles: inputs.writeOutputFiles,
-      outputDir: inputs.outputDir,
-      json: inputs.json
-    })
-    core.info('All Done!')
-    core.endGroup()
-  }
 }
 
-const getChangedFilesFromRESTAPI = async ({
-  inputs,
-  filePatterns,
-  yamlFilePatterns
-}: {
-  inputs: Inputs
-  filePatterns: string[]
-  yamlFilePatterns: Record<string, string[]>
-}): Promise<void> => {
-  const allDiffFiles = await getChangedFilesFromGithubAPI({
-    inputs
-  })
-  core.debug(`All diff files: ${JSON.stringify(allDiffFiles)}`)
-  core.info('All Done!')
+// $ cargo tree --workspace --depth 0
+// hello1 v0.1.0 (/workspaces/codespaces-blank/crates/hello1)
+//
+// hello2 v0.1.0 (/workspaces/codespaces-blank/crates/hello2)
+const { stdout } = await $`cargo tree --workspace --depth 0`
+const packages = stdout
+    .split(/(?:\r?\n){2}/g)
+    .map(x => ({
+        name: x.split(" ", 1)[0],
+        path: x.match(/\((.+)\)/)![1],
+    }))
 
-  await processChangedFiles({
-    filePatterns,
-    allDiffFiles,
-    inputs,
-    yamlFilePatterns
-  })
-}
-
-export async function run(): Promise<void> {
-  core.startGroup('changed-files')
-
-  const env = await getEnv()
-  core.debug(`Env: ${JSON.stringify(env, null, 2)}`)
-
-  const inputs = getInputs()
-  core.debug(`Inputs: ${JSON.stringify(inputs, null, 2)}`)
-
-  core.debug(`Github Context: ${JSON.stringify(github.context, null, 2)}`)
-
-  const workingDirectory = path.resolve(
-    env.GITHUB_WORKSPACE || process.cwd(),
-    inputs.useRestApi ? '.' : inputs.path
-  )
-  core.debug(`Working directory: ${workingDirectory}`)
-
-  const hasGitDirectory = await hasLocalGitDirectory({workingDirectory})
-  core.debug(`Has git directory: ${hasGitDirectory}`)
-
-  const filePatterns = await getFilePatterns({
-    inputs,
-    workingDirectory
-  })
-  core.debug(`File patterns: ${filePatterns}`)
-
-  const yamlFilePatterns = await getYamlFilePatterns({
-    inputs,
-    workingDirectory
-  })
-  core.debug(`Yaml file patterns: ${JSON.stringify(yamlFilePatterns)}`)
-
-  if (inputs.useRestApi && !github.context.payload.pull_request?.number) {
-    throw new Error(
-      "Only pull_request* events are supported when using GitHub's REST API."
-    )
-  }
-
-  if (
-    inputs.token &&
-    github.context.payload.pull_request?.number &&
-    (!hasGitDirectory || inputs.useRestApi)
-  ) {
-    core.info("Using GitHub's REST API to get changed files")
-    await warnUnsupportedRESTAPIInputs({inputs})
-    await getChangedFilesFromRESTAPI({
-      inputs,
-      filePatterns,
-      yamlFilePatterns
-    })
-  } else {
-    if (!hasGitDirectory) {
-      throw new Error(
-        `Unable to locate the git repository in the given path: ${workingDirectory}.\n Please run actions/checkout before this action (Make sure the 'path' input is correct).\n If you intend to use Github's REST API note that only pull_request* events are supported. Current event is "${github.context.eventName}".`
-      )
-    }
-
-    core.info('Using local .git directory')
-    await getChangedFilesFromLocalGitHistory({
-      inputs,
-      env,
-      workingDirectory,
-      filePatterns,
-      yamlFilePatterns
-    })
-  }
-}
-
-/* istanbul ignore if */
-if (!process.env.TESTING) {
-  // eslint-disable-next-line github/no-then
-  run().catch(e => {
-    core.setFailed(e.message || e)
-    process.exit(1)
-  })
-}
+const changedPackages = packages.filter(p => changedFiles.some(f => f.startsWith(p.path + sep)))
+const changedPackageNames = changedPackages.map(x => x.name)
+core.setOutput("changed-packages", JSON.stringify(changedPackageNames))
